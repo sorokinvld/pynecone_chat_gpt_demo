a85c1f06220c50c561cfdb1b607e5f68ee1d72ae
Vladislav Sorokin <sorokinvld@users.noreply.github.com>
2023-05-03 08:37:13 +0300

    Update pcconfig.py

--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,9 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="pc_chat_gpt",
-    port=8000,
+    port=3000,
+    app_name="app",
+    api_url="0.0.0.0:8000",
+    bun_path="/app/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
 )
